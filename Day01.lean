e53a4beaa8111cdb4aa98558ec6667a014e21fa7
mniip <mniip@mniip.com>
2024-12-01 12:18:49 +0100

    NlogN solution

--- Day01.lean
+++ Day01.lean
@@ -1,5 +1,6 @@
 import Aoc2024
 import Init.Data.List.Basic
+import Lean.Data.RBMap
 
 section Parser
 open Parser
@@ -14,8 +15,11 @@ def solution1 : Array (Int × Int) → Nat
       $ Array.zipWith xs.qsortOrd ys.qsortOrd (Int.natAbs $ · - ·)
 
 def solution2 : Array (Int × Int) → Int
-  | input => let (xs, ys) := input.unzip
-    Array.foldl (· + ·) 0 $ xs.map λx => x * ys.toList.count x
+  | input =>
+    let (xs, ys) := input.unzip
+    let counts : Lean.RBMap Int Nat compare
+      := ys.foldl (λm y => m.insert y (m.findD y 0 + 1)) Lean.RBMap.empty
+    Array.foldl (· + ·) 0 $ xs.map λx => x * counts.findD x 0
 
 def main : IO Unit := do
   match parser.parse (← IO.allStdin) with
