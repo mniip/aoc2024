e8d1a531bc7ef0a0e481a7c36959fbd9c92a3a65
mniip <mniip@mniip.com>
2024-12-02 10:37:46 +0100

    Single scan solution

--- Day02.lean
+++ Day02.lean
@@ -8,21 +8,56 @@ def parser : Parser (Array (Array Nat))
   := (((string " ").optional *> nat).many <* string "\n").many
 end Parser
 
+inductive Directed1 (α : Type) where
+  | empty
+  | one (value : α)
+  | failed
+def Directed1.push (pred : α → α → Bool) : Directed1 α → α → Directed1 α
+  | empty, x => one x
+  | one l, x => if pred l x
+    then one x
+    else failed
+  | failed, _ => failed
+
+abbrev Check1 α := Directed1 α × Directed1 α
+def Check1.init : Check1 α := (.empty, .empty)
+def Check1.push (pred : α → α → Bool) : Check1 α → α  → Check1 α
+  | (u, d), x => (u.push pred x, d.push (flip pred) x)
+def Check1.ok : Check1 α → Bool
+  | (.failed, .failed) => false
+  | _ => true
+
+inductive Directed2 (α : Type) where
+  | skipped (_ : Directed1 α)
+  | one (value : α)
+  | two (last last' : α)
+def Directed2.push (pred : α → α → Bool) : Directed2 α → α → Directed2 α
+  | one l, x => two l x
+  | two l' l, x => match pred l' l, pred l' x || pred l x with
+    | true, true => two l x
+    | true, false => skipped (.one l)
+    | false, true => skipped (.one x)
+    | false, false => skipped .failed
+  | skipped .empty, x => one x
+  | skipped s, x => skipped (s.push pred x)
+
+abbrev Check2 α := Directed2 α × Directed2 α
+def Check2.init : Check2 α := (.skipped .empty, .skipped .empty)
+def Check2.push (pred : α → α → Bool) : Check2 α → α → Check2 α
+  | (u, d), x => (u.push pred x, d.push (flip pred) x)
+def Check2.ok : Check2 α → Bool
+  | (.skipped .failed, .skipped .failed) => false
+  | _ => true
+
 def solution1 : Array (Array Nat) → Nat
-  | input => (input.filter λxs => safeFwd xs || safeFwd xs.reverse).size
-  where
-    safeFwd xs := List.all (List.zip xs.toList xs.toList.tail)
-      λ(x, y) => y >= x + 1 && y <= x + 3
+  | input => input.toList.countP
+    $ Check1.ok ∘ Array.foldl (Check1.push pred) Check1.init
+  where pred a b := a + 1 ≤ b && b ≤ a + 3
 
 def solution2 : Array (Array Nat) → Nat
-  | input => (input.filter λxs => safeFwd xs || safeFwd xs.reverse).size
-  where
-    safeFwd ys := List.any (ys.toList :: holes ys.toList)
-      λxs => List.all (List.zip xs xs.tail)
-        λ(x, y) => y >= x + 1 && y <= x + 3
-    holes
-      | x :: xs => xs :: (x :: ·) <$> holes xs
-      | [] => []
+  | input => input.toList.countP
+    $ Check2.ok ∘ Array.foldl (Check2.push pred) Check2.init
+  where pred a b := a + 1 ≤ b && b ≤ a + 3
 
 def main : IO Unit := do
   match parser.parse (← IO.allStdin) with
