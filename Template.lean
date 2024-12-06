22bcac6bc3ad50f2315f53d2aae7310759332e9c
mniip <mniip@mniip.com>
2024-12-06 09:50:37 +0100

    Add template

---
+++ Template.lean
@@ -0,0 +1,14 @@
+import Aoc2024
+
+section Parser
+open Parser
+def parser : Parser Unit := default
+end Parser
+
+def solution1 : Unit → Nat
+  := default
+
+def solution2 : Unit → Int
+  := default
+
+def main : IO Unit := IO.main parser solution1 solution2
